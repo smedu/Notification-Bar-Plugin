62c0fddbd3724950d20d3608c0f01b28929d9fc0
David Laietta <davidjlaietta@gmail.com>
2017-09-30 18:02:40 -0400

    adds customizer settings for notification bar

--- notification-bar.php
+++ notification-bar.php
@@ -175,3 +175,67 @@ function snb_body_class( $classes ) {
     return $classes;
 
 }
+
+
+/**
+ * Adds customizer settings for notification bar
+ */
+add_action( 'customize_register', 'snb_customize_register' );
+function snb_customize_register( WP_Customize_Manager $wp_customize ) {
+
+    $wp_customize->add_section( 'skillshare_notification_bar', array(
+        'title' => __( 'Notification Bar', 'notification-bar' ),
+    ) );
+
+    $wp_customize->add_setting( 'display_location', array(
+        'capability' => 'edit_theme_options',
+        'default' => 'display_none'
+    ) );
+    $wp_customize->add_control( 'display_location', array(
+        'type' => 'radio',
+        'section' => 'skillshare_notification_bar', // Add a default or your own section
+        'label' => __( 'Display Location' ),
+        'description' => __( 'Choose where the notification bar is displayed' ),
+        'choices' => array(
+            'display_none' => __( 'Do not display notification bar', 'notification-bar' ),
+            'display_top' => __( 'Display notification bar on the top of the site', 'notification-bar' ),
+            'display_bottom' => __( 'Display notification bar on the bottom of the site', 'notification-bar' )
+        ),
+    ) );
+
+    $wp_customize->add_setting( 'notification_text', array(
+        'capability' => 'edit_theme_options',
+        'default' => ''
+    ) );
+    $wp_customize->add_control( 'notification_text', array(
+        'type' => 'textarea',
+        'section' => 'skillshare_notification_bar',
+        'label' => __( 'Notification Text' ),
+        'description' => __( 'This is the text of the notification.' ),
+    ) );
+
+    $wp_customize->add_setting( 'notification_bar_color', array(
+        'default'           => '#000000',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'capability'        => 'edit_theme_options',
+
+    ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'notification_bar_color', array(
+        'label'    => __('Notification Bar Color', 'notification-bar'),
+        'section'  => 'skillshare_notification_bar',
+        'settings' => 'notification_bar_color',
+    ) ) );
+
+    $wp_customize->add_setting( 'notification_text_color', array(
+        'default'           => '#FFFFFF',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'capability'        => 'edit_theme_options',
+
+    ) );
+    $wp_customize->add_control( new WP_Customize_Color_Control($wp_customize, 'notification_text_color', array(
+        'label'    => __('Notification Text Color', 'notification-bar'),
+        'section'  => 'skillshare_notification_bar',
+        'settings' => 'notification_text_color',
+    ) ) );
+
+}
